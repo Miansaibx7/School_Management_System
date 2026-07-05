11cf712963d55753ab2a1521775237a0b72caddd
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-05 09:12:15 +0500

    Initial commit

--- school/base/services/salary_service.py
+++ school/base/services/salary_service.py
@@ -6,33 +6,16 @@ from .transaction_service import TransactionService
 class SalaryService:
 
     @staticmethod
-    def pay_salary(teacher,amount,
-        month_for,
-        payment_date,
-        payment_method,
-        paid_by
-    ):
+    def pay_salary(teacher,amount, month_for, payment_date, payment_method, paid_by):
 
         with transaction.atomic():
 
             # Create financial transaction
-            transaction_record = TransactionService.create_expense(
-                title=f"Salary Payment - {teacher.full_name}",
-                category="salary",
-                amount=amount,
-                date=payment_date,
-                user=paid_by
-            )
+            transaction_record = TransactionService.create_expense(title=f"Salary Payment - {teacher.full_name}",
+                category="salary", amount=amount, date=payment_date, user=paid_by)
 
-            salary = Salary.objects.create(
-                teacher=teacher,
-                amount=amount,
-                month_for=month_for,
-                payment_date=payment_date,
-                payment_method=payment_method,
-                transaction=transaction_record,
-                paid_by=paid_by
-            )
+            salary = Salary.objects.create(teacher=teacher, amount=amount, month_for=month_for, payment_date=payment_date,
+                payment_method=payment_method, transaction=transaction_record, paid_by=paid_by)
 
             teacher.update_salary_status()
 
--- school/base/services/transaction_service.py
+++ school/base/services/transaction_service.py
@@ -7,25 +7,11 @@ class TransactionService:
     @staticmethod
     def create_income(title, category, amount, date, user=None, description=""):
 
-        return Transaction.objects.create(
-            title=title,
-            transaction_type="income",
-            category=category,
-            amount=amount,
-            date=date,
-            recorded_by=user,
-            description=description
-        )
+        return Transaction.objects.create(title=title, transaction_type="income", category=category, amount=amount, 
+            date=date, recorded_by=user, description=description)
 
     @staticmethod
     def create_expense(title, category, amount, date, user=None, description=""):
 
-        return Transaction.objects.create(
-            title=title,
-            transaction_type="expense",
-            category=category,
-            amount=amount,
-            date=date,
-            recorded_by=user,
-            description=description
-        )
+        return Transaction.objects.create(title=title, transaction_type="expense", category=category, amount=amount,
+            date=date, recorded_by=user, description=description)
