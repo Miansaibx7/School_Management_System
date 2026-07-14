64c0c4a381f432c13e1b4a1c9608199babeac5b7
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-14 08:20:19 +0500

    Initial commit

--- school/base/views.py
+++ school/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.db.models import Sum,F
+from django.db.models import Sum
 from decimal import Decimal
 import json
 from django.db.models.functions import Coalesce
