e37132eb9553d48cf6fbf82c5b34c5c161498bfb
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-15 08:01:56 +0500

    Initial commit

--- school/base/decorators.py
+++ school/base/decorators.py
@@ -28,7 +28,5 @@ def accountant_required(view_func):
             return view_func(request, *args, **kwargs)
 
         messages.error(request," Access Denied! You don't have permission to access this section.")
-
         return redirect("dashboard")
-
     return wrapper
