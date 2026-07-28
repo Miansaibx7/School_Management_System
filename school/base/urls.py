741a2b2e9f7a56e83d1918429f87d549a75370ba
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-28 20:39:21 +0500

    Initial commit

--- school/base/urls.py
+++ school/base/urls.py
@@ -60,10 +60,10 @@ urlpatterns = [
           path('salaries/update/<int:pk>/', views.salary_update, name='salary_update'),
           path('salaries/delete/<int:pk>/', views.salary_delete, name='salary_delete'),
 
-         # ===================== Financial Reports URLS ====================================
+         # ===================== Financial Reports URLS ==============================
           path('financial-reports/',views.financial_reports,name='financial_reports'),
 
-         # ===================== User CRUD URLS ====================================
+         # ===================== User CRUD URLS =====================================
           path('users/',views.user_list,name='user_list'),
           path('users/create/',views.user_create,name='user_create'),
           path('users/update/<str:pk>/',views.user_update,name='user_update'),
