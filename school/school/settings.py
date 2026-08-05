947681464bba3840ecfe994e358aaaf06acd9dcb
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-05 11:45:07 +0500

    Initial commit

--- school/school/settings.py
+++ school/school/settings.py
@@ -89,9 +89,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static', ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
 import os
 MEDIA_URL = '/media/' # For user upload Images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
