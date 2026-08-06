1f9ec1e521ed6d6ebb4f529beb0c46f55f28fec1
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-06 23:22:39 +0500

    Initial commit

--- school/school/settings.py
+++ school/school/settings.py
@@ -9,8 +9,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
