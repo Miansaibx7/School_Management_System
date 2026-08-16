0c4f29203424bd8793bc0a2224da33c043fda771
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-16 11:02:11 +0500

    Initial commit

--- school/base/urls.py
+++ school/base/urls.py
@@ -7,11 +7,8 @@ urlpatterns = [
     # ===================== Transactions URLS =====================
     path("transactions/", views.transaction_list, name="transaction_list"),
     path("transactions/create/", views.transaction_create, name="transaction_create"),
-    path(
-        "transactions/update/<int:pk>/",
-        views.transaction_update,
-        name="transaction_update",
-    ),
+    path("transactions/update/<int:pk>/", views.transaction_update,
+        name="transaction_update"),
     path(
         "transactions/delete/<int:pk>/",
         views.transaction_delete,
