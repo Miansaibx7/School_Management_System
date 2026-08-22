64c3d62f0908bb900af379c89bb6347016353744
mian muhammad waqas <mianmwaqas77@gmail.com>
2026-08-22 12:08:33 +0300

    Initial commit

--- school/base/decorators.py
+++ school/base/decorators.py
@@ -13,7 +13,7 @@ def admin_required(view_func):
         if request.user.is_admin:
             return view_func(request, *args, **kwargs)
 
-        messages.error(request, " Access Denied! You don't have permission to access this section.")
+        messages.error(request, "Access Denied! You don't have permission to access this section.")
         return redirect("dashboard")
 
     return wrapper
@@ -30,7 +30,7 @@ def accountant_required(view_func):
         if request.user.is_accountant:
             return view_func(request, *args, **kwargs)
 
-        messages.error(request, " Access Denied! You don't have permission to access this section.")
+        messages.error(request, "Access Denied! You don't have permission to access this section.")
         return redirect("dashboard")
 
     return wrapper
